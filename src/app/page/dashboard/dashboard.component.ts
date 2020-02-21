08a986369ea39276472d00ff2e58ec59a1bac107
Aldi Yusron <mhmmduso@gmail.com>
2020-02-21 20:45:13 +0700

    update ui & layout

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDate, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  showCalendar: boolean;
+
+  period: string;
+  dateString: string;
+  faAngleDown = faAngleDown;
+
+  startDate: Date;
+  endDate: Date;
+
+  options = { year: 'numeric', month: 'long', day: 'numeric' };
+
+  periodList: string[] = ['Today', 'Yesterday', 'Last 7 Days', 'Last 30 Days', 'This Month', 'Custom'];
+
+  constructor() { 
+    this.showCalendar = false;
+  }
 
   ngOnInit(): void {
+    this.setPeriod(this.periodList[2]);
+  }
+
+  setPeriod(period: string) {
+    this.period = period;
+    this.setDate(period);
+  }
+
+  setDate(period: string) {
+    if(period.toLowerCase() == 'today') {
+      this.startDate = new Date();
+      this.dateString = this.startDate.toLocaleDateString('id-ID', this.options);
+    } else if(period.toLowerCase() == 'yesterday') {
+      this.startDate = new Date();
+      this.startDate.setDate(this.startDate.getDate() - 1);
+      this.dateString = this.startDate.toLocaleDateString('id-ID', this.options);
+    } else if(period.toLowerCase() == 'last 7 days') {
+      this.startDate = new Date();
+      this.startDate.setDate(this.startDate.getDate() - 1);
+      this.endDate = new Date();
+      this.endDate.setDate(this.endDate.getDate() - 7);
+      this.dateString = `${this.startDate.toLocaleDateString('id-ID', this.options)} - ${this.endDate.toLocaleDateString('id-ID', this.options)}`;
+    } else if(period.toLowerCase() == 'last 30 days') {
+      this.startDate = new Date();
+      this.startDate.setDate(this.startDate.getDate() - 1);
+      this.endDate = new Date();
+      this.endDate.setDate(this.endDate.getDate() - 30);
+      this.dateString = `${this.startDate.toLocaleDateString('id-ID', this.options)} - ${this.endDate.toLocaleDateString('id-ID', this.options)}`;
+    } else if(period.toLowerCase() == 'this month') {
+      let date = new Date();
+      this.startDate = new Date(date.getFullYear(), date.getMonth(), 1);
+      this.endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+      this.dateString = `${this.startDate.toLocaleDateString('id-ID', this.options)} - ${this.endDate.toLocaleDateString('id-ID', this.options)}`;
+    }
+  }
+
+  checkMin(start: Date, end: Date): boolean {
+    return true;
+  }
+
+  checkMax(start: Date, end: Date): boolean {
+    return true;
   }
 
 }
